bc53b94585f495ff223b74fbb2a0674f3ba7d5fb
heohailua <105148960+heohailua@users.noreply.github.com>
2022-05-08 16:18:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: " =)) ",
+    titleWeb: " tỏ tình !! ",
     introTitle: 'Trần Quốc Khánh * Khénh * ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
